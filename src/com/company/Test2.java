2d10d4b447d1305d382fb22e5787ce83e9858041
TravelerRtB <43881304+TravelerRtB@users.noreply.github.com>
2021-05-03 01:17:56 +0200

    Just for testing

--- src/com/company/Test2.java
+++ src/com/company/Test2.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Test2 {
     public static void main(String[] args) {
-        String message = greetUser ("Nejad", "Muhammad");
+        String message = greetUser ("John", "Wich");
         System.out.println(message);
     }
     public static String greetUser(String firstName, String lastName){
