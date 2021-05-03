ab091007ddfe8e11d65f19725ecee1f63c48b89b
TravelerRtB <43881304+TravelerRtB@users.noreply.github.com>
2021-05-03 03:29:46 +0200

    Just for testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         var textBox1 = new TextBox();
         textBox1.setText("This is Box 1");
-        System.out.println(textBox1.text);
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("This is the 2nd box");
+        System.out.println(textBox2.text.toUpperCase());
     }
 }
