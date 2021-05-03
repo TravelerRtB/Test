cf8a95e2485081a0f01da4fe7eb6f6733c99e9f2
TravelerRtB <43881304+TravelerRtB@users.noreply.github.com>
2021-05-03 03:36:33 +0200

    Just for testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         var textBox1 = new TextBox();
-        textBox1.setText("This is Box 1");
+        //textBox1.setText("This is Box 1");
         System.out.println(textBox1.text.toUpperCase());
 
         var textBox2 = new TextBox();
--- src/com/company/TextBox.java
+++ src/com/company/TextBox.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class TextBox {
-    public String text = "";
+    public String text = "No text in this line was set yet";
 
     public void setText(String text) {
         this.text = text;
